39c4687e442e608e88ded44ca8ee3dc0a72420bc
Natasha Murashev <nmurashev@gmail.com>
2014-05-26 08:57:20 -0700

    added tableview to the cell

--- RecentContacts/NTRContactCollectionViewCell.m
+++ RecentContacts/NTRContactCollectionViewCell.m
@@ -9,8 +9,9 @@
 #import "NTRContactCollectionViewCell.h"
 #import "UIImage+ImageEffects.h"
 #import "NTRContact+NTRExtensions.h"
+#import "NTRContactPhoneTableViewCell.h"
 
-@interface NTRContactCollectionViewCell ()
+@interface NTRContactCollectionViewCell () <UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
 @property (weak, nonatomic) IBOutlet UIImageView *contactImageView;
@@ -23,6 +24,11 @@
 - (void)prepareForReuse
 {
     self.contactImageView.image = nil;
+}
+
+- (void)awakeFromNib
+{
+    [self registerTableViewViewCellsFromNib];
 }
 
 #pragma mark - Setter Overrides
@@ -45,6 +51,48 @@
         self.contactImageView.layer.cornerRadius = self.contactImageView.frame.size.width / 2;
         self.contactImageView.layer.masksToBounds = YES;
     }
+}
+
+#pragma mark - Table View Data Source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSInteger numberOfRows = 0;
+    
+    if(self.contact.compositeName) {
+        numberOfRows ++;
+    }
+    
+    if(self.contact.emails) {
+        numberOfRows += [self.contact.emails count];
+    }
+    
+    if(self.contact.phones) {
+        numberOfRows += [self.contact.phones count];
+    }
+    
+    return numberOfRows;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NTRContactPhoneTableViewCell *phoneCell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([NTRContactPhoneTableViewCell class])];
+    phoneCell.textLabel.text = @"hello";
+    
+    return phoneCell;
+}
+
+#pragma mark - Private Helper Classes
+
+- (void)registerTableViewViewCellsFromNib
+{
+    NSString *cellIdentifier = NSStringFromClass([NTRContactPhoneTableViewCell class]);
+    [self.tableView registerNib:[UINib nibWithNibName:cellIdentifier bundle:nil] forCellReuseIdentifier:cellIdentifier];
 }
 
 @end
---
+++ RecentContacts/NTRContactPhoneTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NTRContactPhoneTableViewCell.h
+//  RecentContacts
+//
+//  Created by Natasha Murashev on 5/26/14.
+//  Copyright (c) 2014 NatashaTheRobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTRContactPhoneTableViewCell : UITableViewCell
+
+@end
